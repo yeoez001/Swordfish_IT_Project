8910a0672197f59ca294c268f7d64fbecd86980c
yeoez001 <yeoez001@mymail.unisa.edu.au>
2022-05-03 15:30:19 +0930

    Added test slider button UI interaction

---
+++ Assets/Tilia.UnityUI-2.0.1/Tilia.UnityUI-2.0.1/README.md
@@ -0,0 +1,89 @@
+[![Tilia logo][Tilia-Image]](#)
+
+> ### Tilia Unity UI Unity Editor 2017 +
+> Unity UI with UI Pointers.
+
+[![Release][Version-Release]][Releases]
+[![License][License-Badge]][License]
+
+## Introduction
+
+Support for Unity UI. Both New and Old input systems
+
+![FullSetup](./Documentation/HowToGuides/Installation/Capture.PNG)
+
+
+> **Requires** Unity Editor 2017.4 +
+
+![Full_Animation]
+
+
+## Important usage
+
+- Place VRTK4_UICanvas on each canvas
+- ON ALL UI ELEMENTS - buttons, sliders, inputfields, toggles, dropdown -> YOU MUST REMOVE DEFAULT UNITY NAVIGATION (navigation = none)!
+- You may leave Blocking mask as None (this way you will save on performance - no 3D raycasts will be performed)
+- To Ignore Player Objects - Use Component VRTK4_Player Object (collision detection)
+- Also, if you are using Pseudobody - add pointers to the IgnoredGameObjectList
+- when using animations on the UI canvas -> add box collider on it in Editor mode -> Colliders are created at the awake - so it can be different when the target canvas is animated!
+- When using Unity Event Triggers -> pointers will always perform their separate OnEnter/OnExit -> so please see Unity Event Triggers usage script
+- All input fields must hide soft keyboard and hide mobile input. There is no default keyboard in this package. Please Use Custom one.
+- When using input fields -> VR + Input Field Caret is not working properly. Make sure to use custom caret. Example See here -> https://gist.github.com/studentutu/714d78ad6cebfe147bfa852fb7046a90
+    
+
+## Getting Started
+
+Please refer to the [installation] guide to install this package.
+
+See full scene and a separate prefab here :
+
+/Runtime/Example/TestScene -> here is a full scene with Custom Rig and UI Pointers attached
+
+/Runtime/SharedResources/Prefabs/[L_R]_ UI Pointer on Interactor.prefab  -> default UI Pointer, needs additional setup with Pointer, actions and PointerFacade
+
+/Runtime/SharedResources/Prefabs/Full_L_Interactions.Interactor Variant.prefab -> FUll Tilia Pointer with attached UI Pointer
+
+
+## Documentation
+
+Please refer to the [How To Guides] for usage of this package.
+
+Further documentation can be found within the [Documentation] directory and at https://academy.vrtk.io
+
+## Contributing
+
+Please refer to the Extend Reality [Contributing guidelines] and the [project coding conventions].
+
+## Code of Conduct
+
+Please refer to the Extend Reality [Code of Conduct].
+
+## License
+
+Code released under the [MIT License][License].
+
+[License-Badge]: https://img.shields.io/github/license/ExtendRealityLtd/Tilia.Utilities.Shaders.Unity.svg
+
+[Version-Release]: https://img.shields.io/badge/package-v1.7.0-blue
+
+[project coding conventions]: https://github.com/ExtendRealityLtd/.github/blob/master/CONVENTIONS/UNITY3D.md
+
+[Tilia-Image]: https://user-images.githubusercontent.com/1029673/67681496-5bf10700-f985-11e9-9413-e61801b6eab5.png
+
+[License]: LICENSE.md
+
+[Documentation]: Documentation/
+
+[How To Guides]: Documentation/HowToGuides/
+
+[Installation]: Documentation/HowToGuides/Installation/README.md
+
+[Backlog-Badge]: https://img.shields.io/badge/project-backlog-78bdf2.svg
+
+[Releases]: ../../releases
+
+[Contributing guidelines]: https://github.com/ExtendRealityLtd/.github/blob/master/CONTRIBUTING.md
+
+[Code of Conduct]: https://github.com/ExtendRealityLtd/.github/blob/master/CODE_OF_CONDUCT.md
+
+[Full_Animation]: ./Documentation/HowToGuides/Installation/Animation.gif
---
+++ Assets/Tilia.UnityUI-2.0.1/Tilia.UnityUI-2.0.1/Runtime/SharedResources/Scripts/VRTK3.3.0 UI/VRTK4_UIGraphicRaycaster.cs
@@ -0,0 +1,236 @@
+﻿namespace Tilia.VRTKUI
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// This script allows VRTK to interact cleanly with Unity Canvases.
+    /// It is mostly a duplicate of Unity's default GraphicsRaycaster:
+    /// https://bitbucket.org/Unity-Technologies/ui/src/0155c39e05ca5d7dcc97d9974256ef83bc122586/UnityEngine.UI/UI/Core/GraphicRaycaster.c
+    /// However, it allows for graphics to be hit when they are not in view of a camera.
+    /// Note: Not intended for direct use. VRTK will intelligently replace the default GraphicsRaycaster
+    ///   on canvases with this raycaster.
+    /// </summary>>
+    [DisallowMultipleComponent]
+    public class VRTK4_UIGraphicRaycaster : GraphicRaycaster
+    {
+        public static VRTK4_UIPointer CurrentPointer;
+        private static RaycastHit[] st_arrayOfRaycasts = new RaycastHit[10];
+        private static RaycastHit2D[] st_arrayOfRaycasts2d = new RaycastHit2D[10];
+        protected Canvas currentCanvas = null;
+        protected const float UI_CONTROL_OFFSET = 0.00001f;
+
+        // Use a static to prevent list reallocation. We only need one of these globally (single main thread), and only to hold temporary data
+        [NonSerialized] private static List<RaycastResult> s_RaycastResults = new List<RaycastResult>();
+
+        protected virtual Canvas CanvasToUse
+        {
+            get
+            {
+                if (currentCanvas != null)
+                {
+                    return currentCanvas;
+                }
+
+                currentCanvas = gameObject.GetComponent<Canvas>();
+                return currentCanvas;
+            }
+        }
+
+        /// <summary>
+        /// Enables to raycast event from any point towards any VRTK4_Canvas (even when outside the view)
+        /// </summary>
+        /// <param name="eventData"> currentEventData (can get cleared in consequent stages) </param>
+        /// <param name="resultAppendList"> actual hits on UI elements under canvases</param>
+        public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList)
+        {
+            if (CanvasToUse == null || eventCamera == null || !eventCamera.enabled)
+            {
+                return;
+            }
+
+            Ray ray;
+            if (CurrentPointer != null)
+            {
+                ray = new Ray(CurrentPointer.GetOriginPosition(), CurrentPointer.GetOriginForward());
+            }
+            else
+            {
+                ray = new Ray(eventData.pointerCurrentRaycast.worldPosition,
+                    eventData.pointerCurrentRaycast.worldNormal);
+            }
+
+            s_RaycastResults.Clear();
+            Raycast(CanvasToUse, eventCamera, eventData, ray, ref s_RaycastResults);
+            AppendToListAllCurrentRaycasts(ref resultAppendList, ref s_RaycastResults);
+            s_RaycastResults.Clear();
+        }
+
+        /// <summary>
+        /// Fills the list of cast rays
+        /// </summary>
+        protected virtual void AppendToListAllCurrentRaycasts(ref List<RaycastResult> resultAppendList,
+            ref List<RaycastResult> raycastResults)
+        {
+            int totalCount = raycastResults.Count;
+            for (int index = 0; index < totalCount; index++)
+            {
+                VRTK4_SharedMethods.AddListValue(resultAppendList, raycastResults[index]);
+            }
+        }
+
+        // Skip check for near/far plane!
+        protected virtual float GetHitDistance(Ray ray, float hitDistance)
+        {
+            if (CanvasToUse.renderMode != RenderMode.ScreenSpaceOverlay && blockingObjects != BlockingObjects.None)
+            {
+                float maxDistance = Vector3.Distance(ray.origin, CanvasToUse.transform.position) + 10f;
+                int allraycasts = -1;
+                if (blockingObjects == BlockingObjects.ThreeD || blockingObjects == BlockingObjects.All)
+                {
+                    allraycasts = Physics.RaycastNonAlloc(ray, st_arrayOfRaycasts, maxDistance, m_BlockingMask);
+                    if (allraycasts > 0)
+                    {
+                        RaycastHit hit = st_arrayOfRaycasts[0];
+                        if (hit.collider != null && !VRTK4_PlayerObject.IsPlayerObject(hit.collider.gameObject))
+                        {
+                            hitDistance = hit.distance;
+                        }
+                    }
+
+                    ClearArrNonAlloc3D();
+                }
+
+                if (blockingObjects == BlockingObjects.TwoD || blockingObjects == BlockingObjects.All)
+                {
+                    allraycasts = Physics2D.RaycastNonAlloc(ray.origin, ray.direction, st_arrayOfRaycasts2d,
+                        maxDistance,
+                        m_BlockingMask);
+                    if (allraycasts > 0)
+                    {
+                        RaycastHit2D hit = st_arrayOfRaycasts2d[0];
+                        if (hit.collider != null && !VRTK4_PlayerObject.IsPlayerObject(hit.collider.gameObject))
+                        {
+                            hitDistance = hit.fraction * maxDistance;
+                        }
+                    }
+
+                    ClearArrNonAlloc2D();
+                }
+            }
+
+            return hitDistance;
+        }
+
+        private static void ClearArrNonAlloc3D()
+        {
+            for (int i = 0; i < st_arrayOfRaycasts.Length; i++)
+            {
+                st_arrayOfRaycasts[i] = default;
+            }
+        }
+
+        private static void ClearArrNonAlloc2D()
+        {
+            for (int i = 0; i < st_arrayOfRaycasts2d.Length; i++)
+            {
+                st_arrayOfRaycasts2d[i] = default;
+            }
+        }
+
+        /// <summary>
+        /// Changes - ignore displays. Improved Performance : no need to sort graphics only, we can sort all raycast once.
+        /// </summary>
+        /// <param name="canvasIn"></param>
+        /// <param name="eventCameraIn"></param>
+        /// <param name="eventData"></param>
+        /// <param name="ray"></param>
+        /// <param name="helperList"></param>
+        protected virtual void Raycast(Canvas canvasIn, Camera eventCameraIn, PointerEventData eventData, Ray ray,
+            ref List<RaycastResult> helperList)
+        {
+            // bool checkRaycastGraphic = false;
+            float hitDistance = GetHitDistance(ray, VRTK4_UIPointer.GetPointerLength(eventData.pointerId));
+            IList<Graphic> canvasGraphics = null;
+#if UNITY_2020_3_OR_NEWER
+            canvasGraphics = GraphicRegistry.GetRaycastableGraphicsForCanvas(canvasIn);
+#else
+            canvasGraphics = GraphicRegistry.GetGraphicsForCanvas(canvasIn);
+#endif
+            int totalCount = canvasGraphics.Count;
+            for (int i = 0; i < totalCount; ++i)
+            {
+                Graphic graphic = canvasGraphics[i];
+
+                if (graphic.depth == -1 || !graphic.raycastTarget || graphic.canvasRenderer.cull)
+                {
+                    continue;
+                }
+
+                Transform graphicTransform = graphic.transform;
+                Vector3 graphicForward = graphicTransform.forward;
+                float distance = Vector3.Dot(graphicForward, graphicTransform.position - ray.origin) /
+                                 Vector3.Dot(graphicForward, ray.direction);
+
+                if (distance < 0)
+                {
+                    continue;
+                }
+
+                //Prevents "flickering hover" on items near canvas center.
+                if ((distance - UI_CONTROL_OFFSET) > hitDistance)
+                {
+                    continue;
+                }
+
+                Vector3 position = ray.GetPoint(distance);
+                Vector2 pointerPosition = eventCameraIn.WorldToScreenPoint(position);
+#if UNITY_2020_3_OR_NEWER
+                if (!RectTransformUtility.RectangleContainsScreenPoint(
+                    graphic.rectTransform,
+                    pointerPosition,
+                    eventCameraIn, graphic.raycastPadding))
+                {
+                    continue;
+                }
+
+#else
+                if (!RectTransformUtility.RectangleContainsScreenPoint(
+                    graphic.rectTransform,
+                    pointerPosition,
+                    eventCameraIn))
+                {
+                    continue;
+                }
+#endif
+
+                if (graphic.Raycast(pointerPosition, eventCameraIn))
+                {
+                    RaycastResult result = new RaycastResult()
+                    {
+                        gameObject = graphic.gameObject,
+                        module = this,
+                        distance = distance,
+                        worldPosition = position,
+                        worldNormal = -graphicForward,
+                        screenPosition = pointerPosition,
+                        depth = graphic.depth + canvasIn.sortingLayerID * 1000 + canvasIn.sortingOrder,
+                        sortingLayer = canvasIn.sortingLayerID,
+                        sortingOrder = canvasIn.sortingOrder,
+                    };
+                    VRTK4_SharedMethods.AddListValue(helperList, result);
+                }
+            }
+
+            helperList.Sort(ComparisonInversed);
+        }
+
+        private static int ComparisonInversed(RaycastResult g1, RaycastResult g2)
+        {
+            return g2.depth.CompareTo(g1.depth);
+        }
+    }
+}
